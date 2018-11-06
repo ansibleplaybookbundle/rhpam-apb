5754489abde6a41787729b320d4afe4a388420ad
Ruben Romero Montes <rromerom@redhat.com>
2018-11-06 11:19:21 +0100

    [RHPAM-1652] Build using cekit only
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-apb bundle prepare -nc Dockerfile-latest > /dev/null 1>&1
-encoded_apb=$(sed -n '/com.redhat.apb.spec/{n;p;}' Dockerfile-latest)
-
-for dockerfile in canary latest nightly
-do
-    echo "Prepare Dockerfile-$dockerfile"
-    apb bundle prepare -c Dockerfile-$dockerfile > /dev/null 1>&1
-    echo "Done"
-done
+encoded_apb=$(base64 -w 0 apb.yml)
 
 echo "Update image.yaml file with b64 encoded spec"
-sed -i "/com.redhat.apb.spec/!b;n;c\ \ \ \ value: ${encoded_apb}" image.yaml
+sed -i "/com.redhat.apb.spec/!b;n;c\ \ \ \ value: \"${encoded_apb}\"" image.yaml
 echo "Finished prepare"
