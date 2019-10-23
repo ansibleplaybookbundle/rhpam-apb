e955551e9c8363e0393b7838fb7a5e0e5cf9ca99
David Ward <errantepiphany@users.noreply.github.com>
2019-10-23 11:33:46 -0400

    [KIECLOUD-332] image naming convention changes (#112)
    Signed-off-by: David Ward <dward@redhat.com>

--- prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Encode the apb.yml file and break lines maxing out 512 characters per line.
-# Additionally append slashes at the end, and remove last slashes
-encoded_apb=$(base64 -w 512 apb.yml | sed 's/$/\\\\\\/g' | sed '$ s/...$//')
-
-echo "Update image.yaml file with b64 encoded spec"
-perl -i -0pe "s/name: \"com.redhat.apb.spec\"\n    value: \".*\"/name: \"com.redhat.apb.spec\"\n    value: \"${encoded_apb}\"/gms" image.yaml
-echo "Finished prepare"
